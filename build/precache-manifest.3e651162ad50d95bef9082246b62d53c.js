af42af5361cc5a4ba5d69ca396fbada00213b6e9
Himanshu Yadav <86050941+SeriousConjurer@users.noreply.github.com>
2021-10-10 21:56:58 +0530

    Add files via upload

---
+++ build/precache-manifest.3e651162ad50d95bef9082246b62d53c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71206e20af1bd8bb2d61d4f89348601d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7623fda8ba3f5f21a04e",
+    "url": "/static/css/main.840f7c97.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7623fda8ba3f5f21a04e",
+    "url": "/static/js/main.20c4a615.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
